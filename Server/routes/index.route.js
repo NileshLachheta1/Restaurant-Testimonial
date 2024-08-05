67e8cf97183f4b1b838be748539be25e11b2e316
xyz <xyz@gmail.com>
2024-08-05 23:20:41 +0530

    add frontend part to deploy

--- Server/routes/index.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import {contactUsForm , disheslistController,orderController} from '../controllers/index.controller.js';
-var Router = express.Router();
-
-Router.post("/contact",contactUsForm);
-Router.get("/disheslist",disheslistController);
-Router.post('/placeorder',orderController);
-export default Router;
